ddf75f71e0bf2035c7b28126376fd0f1fac77637
alex <adalyac@gmail.com>
2014-05-26 17:02:56 +0200

    mv install files to decaf/install

--- determine_boost_version.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/version.hpp>
-#include <iostream>
-#include <iomanip>
-
-int main()
-{
-    std::cout << "Boost version: " << BOOST_LIB_VERSION;
-    return 0;
-}
--- makefile
+++
@@ -1,2 +0,0 @@
-boost_version: determine_boost_version.cpp
-	g++ -Wall -g determine_boost_version.cpp -o boost_version
