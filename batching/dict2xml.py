66accce59e4489c3db5842c531dcf1a341feeb97
alex <adalyac@gmail.com>
2014-05-30 19:19:07 +0200

    adding dict2xml.py

---
+++ batching/dict2xml.py
@@ -0,0 +1,42 @@
+from xml.dom.minidom import Document
+import copy
+
+class dict2xml(object):
+    doc     = Document()
+
+    def __init__(self, structure):
+        if len(structure) == 1:
+            rootName    = str(structure.keys()[0])
+            self.root   = self.doc.createElement(rootName)
+
+            self.doc.appendChild(self.root)
+            self.build(self.root, structure[rootName])
+
+    def build(self, father, structure):
+        if type(structure) == dict:
+            for k in structure:
+                tag = self.doc.createElement(k)
+                father.appendChild(tag)
+                self.build(tag, structure[k])
+
+        elif type(structure) == list:
+            grandFather = father.parentNode
+            tagName     = father.tagName
+            grandFather.removeChild(father)
+            for l in structure:
+                tag = self.doc.createElement(tagName)
+                self.build(tag, l)
+                grandFather.appendChild(tag)
+
+        else:
+            data    = str(structure)
+            tag     = self.doc.createTextNode(data)
+            father.appendChild(tag)
+
+    def display(self):
+        print self.doc.toprettyxml(indent="  ")
+
+if __name__ == '__main__':
+    example = {'auftrag':{"kommiauftragsnr":2103839, "anliefertermin":"2009-11-25", "prioritaet": 7,"ort": u"Huecksenwagen","positionen": [{"menge": 12, "artnr": "14640/XL", "posnr": 1},],"versandeinweisungen": [{"guid": "2103839-XalE", "bezeichner": "avisierung48h","anweisung": "48h vor Anlieferung unter 0900-LOGISTIK avisieren"},]}}
+    xml = dict2xml(example)
+    xml.display()
