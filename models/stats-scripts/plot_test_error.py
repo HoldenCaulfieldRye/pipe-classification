1e931f1d1c5bef37f94f56566963d70d8b9fcca9
alex <adalyac@gmail.com>
2014-06-18 11:56:14 +0100

    script working, now using all train errors and step-like test errors

--- models/stats-scripts/plot_test_error.py
+++ models/stats-scripts/plot_test_error.py
@@ -2,6 +2,7 @@ import cPickle as pickle
 import sys, os, shutil, re
 import numpy as np
 import matplotlib.pyplot as plt
+from math import ceil
 
 
 if __name__ == '__main__':
@@ -9,41 +10,43 @@ if __name__ == '__main__':
   train_path = sys.argv[1]
   cfg_dir, train_output_fname = os.path.split(os.path.normpath(train_path))
 
-  test_series = []
-  prev_testoutput = False
   with open(train_path) as f:
     content = f.readlines()
 
     # get test error
-    for line in content:
-      if prev_testoutput:
-        strnum = line.split(',')[0]
-        strnum = strnum.split('  ')[-1]
-        num = float(strnum)
-        test_series.append(strnum)
-        prev_testoutput = False
-      elif '===Test output===' in line: prev_testoutput = True
-      continue
+    # for line in content:
+      # if prev_testoutput:
+      #   strnum = line.split(',')[0]
+      #   strnum = strnum.split('  ')[-1]
+      #   num = float(strnum)
+      #   test_series.append(strnum)
+      #   prev_testoutput = False
+      # elif '===Test output===' in line: prev_testoutput = True
+      # continue
       
     # get train error time averaged over two test error computations
-    train_series = []
+    error = []
+    test_error = 1 # blank would be better for matplotlib
     for idx in xrange(len(content)):
+      
       if content[idx].startswith("Testing "):
+        
         if content[idx].startswith("Testing frequency"):
           test_freq = int(content[idx].strip().split(' ')[-1])
           print "test_freq changed to %i"%(test_freq)
-        else:
-          l = [float(content[i].strip().split(' ')[3].split(',')[0]) 
-               for i in range(idx-test_freq-1,idx-1)]
-          avg_train_error = reduce(lambda x,y: x+y, l) / len(l)
-          train_series.append(str(avg_train_error))
 
-    assert len(test_series) == len(train_series)
-    pretty_print = zip(train_series, test_series)
+        else:
+          for i in range(idx-test_freq-1,idx-1):
+            error.append((float(content[i].strip().split(' ')[3].split(',')[0]),
+                          test_error))
+
+      if "===Test output===" in content[idx]:
+        test_error = float(content[idx+1].split(',')[0].split('  ')[-1])
+
 
   data = open(os.getcwd()+'/time_series.txt','w')
-  data.writelines(["%i\t%s\t%s\n" % (x,train,test)
-                   for x,(train,test) in enumerate(pretty_print)])
+  data.writelines(["%i\t%.5f \t%.5f\n" % (x,train,test)
+                   for x,(train,test) in enumerate(error)])
   print "wrote txt data to %s"%(os.getcwd()+'/time_series.txt')
   data.close()
 
