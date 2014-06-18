e36136c3a0dead0a1914fedb1719414a9db3ae27
alex <adalyac@gmail.com>
2014-06-18 20:21:07 +0100

    about to heed g+ advice and train 10-06 again with smaller learning rate

--- models/stats-scripts/plot_test_error.py
+++ models/stats-scripts/plot_test_error.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 from math import ceil
 
   
-def write_data_to_txt(error):  
+def write_data_to_txt(error, num_epochs=-1):
+  if num_epochs == -1: end = len(error)
+  else: end = num_epochs*800
   data = open(os.getcwd()+'/time_series.txt','w')
   data.writelines(["%i\t%.5f \t%.5f\n" % (x,train,test)
-                   for x,(train,test) in enumerate(error)])
+                   for x,(train,test) in enumerate(error[0:end])])
   print "wrote txt data to %s"%(os.getcwd()+'/time_series.txt')
   data.close()
 
@@ -35,14 +37,21 @@ def parse(content):
   return error      
 
 
-def matplot(cfg_dir, error):
-  x = np.array(range(len(error)))
-  ytrain = np.array([train for (train,test) in error])
-  ytest = np.array([test for (train,test) in error])
-  plt.plot(x, ytrain)
-  plt.plot(x, ytest)
+def matplot(cfg_dir, error, num_epochs=-1):
+  if num_epochs == -1:
+    end = len(error)
+    print 'plotting entire training data'
+  else:
+    end = num_epochs*800
+    print 'plotting %i epochs' % (num_epochs)
+  x = np.array(range(len(error[:end])))
+  ytrain = np.array([train for (train,test) in error[:end]])
+  ytest = np.array([test for (train,test) in error[:end]])
+  plt.plot(x, ytrain, label='training error')
+  plt.plot(x, ytest, label='validation error')
+  plt.legend(loc='upper left')
   plt.xlabel('minibatch passes')
-  plt.ylabel('error rate')
+  plt.ylabel('% error rate')
   # plt.title('Single-Input Logistic Sigmoid Neuron')
   plt.grid(True)
   plt.savefig(cfg_dir+"/plot_time_series_error_rates.png")
@@ -59,6 +68,9 @@ if __name__ == '__main__':
     content = f.readlines()
     error = parse(content)
 
-  matplot(cfg_dir, error)
-
+  try:
+    matplot(cfg_dir, error, int(sys.argv[2]))
+  except:
+    matplot(cfg_dir, error)
+    
   # gnuplot(cfg_dir)
