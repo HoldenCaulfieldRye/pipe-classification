287e2787b7470509772ab67eda4e1a45b796dad7
HoldenCaulfieldICL <adalyac@gmail.com>
2014-06-19 20:50:45 +0100

    modified scp_here script

--- noccn/noccn/scp_graphic02_here.sh
+++ noccn/noccn/scp_graphic02_here.sh
@@ -8,8 +8,10 @@ mkdir /data/ad6813/pipe-data/Redbox/batches
 mkdir /data/ad6813/pipe-data/Redbox/batches/clamp_detection
 mkdir /data/ad6813/my-nets
 mkdir /data/ad6813/my-nets/saves/
+cd /data/ad6813/pipe-data/Redbox/batches/clamp_detection
 echo "copying batches from graphic02 to local data storage..."
-scp graphic02.doc.ic.ac.uk:/data2/ad6813/pipe-data/Redbox/batches/clamp_detection/* /data/ad6813/pipe-data/Redbox/batches/clamp_detection/
+scp graphic02.doc.ic.ac.uk:/data2/ad6813/pipe-data/Redbox/batches/clamp_detection/* .
 echo "copying ConvNet 23.05.49 from graphic02 to local data storage..."
-scp graphic02.doc.ic.ac.uk:/data2/ad6813/my-nets/saves/ConvNet__2014-06-17_23.05.49 /data/ad6813/my-nets/saves/
+cd /data/ad6813/my-nets/saves/
+scp graphic02.doc.ic.ac.uk:/data2/ad6813/my-nets/saves/ConvNet__2014-06-17_23.05.49 .
 echo "WARNING: modify options.cfg to look up in /data, not /data2"
